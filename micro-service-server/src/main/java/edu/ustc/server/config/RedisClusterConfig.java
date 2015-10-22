6d98c70daeb3a0b8262634d092837d21e12797a5
colddew <88028842@qq.com>
2015-10-23 02:25:37 +0800

    modify redis cluster configuration

--- micro-service-server/src/main/java/edu/ustc/server/config/RedisClusterConfig.java
+++ micro-service-server/src/main/java/edu/ustc/server/config/RedisClusterConfig.java
@@ -1,10 +1,20 @@
 package edu.ustc.server.config;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.Set;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.AbstractEnvironment;
+import org.springframework.core.env.CompositePropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.core.env.PropertySource;
+import org.springframework.util.StringUtils;
 
 import redis.clients.jedis.HostAndPort;
 import redis.clients.jedis.JedisCluster;
@@ -12,15 +22,50 @@ import redis.clients.jedis.JedisCluster;
 @Configuration
 public class RedisClusterConfig {
 	
+	public static final String CLUSTER_NODES = "redis.cluster.nodes";
+	public static final String TIMEOUT = "redis.timeout";
 	public static final String DELIMITER_SEMICOLON = ";";
 	public static final String DELIMITER_COLON = ":";
-	public static final String CLUSTER_NODES = "127.0.0.1:7000;127.0.0.1:7001;127.0.0.1:7002;127.0.0.1:7003;127.0.0.1:7004;127.0.0.1:7005";
-	public static final int TIMEOUT = 5000;
+	
+	@Autowired
+    private Environment env;
 	
 	@Bean
 	public JedisCluster jedisCluster() {
-		return new JedisCluster(configRedisCluster(CLUSTER_NODES), TIMEOUT);
+		
+		Map<String, Object> map = loadProperties();
+        if(StringUtils.isEmpty(map.get(CLUSTER_NODES)) || StringUtils.isEmpty(map.get(TIMEOUT))) {
+			throw new RuntimeException("load redis properties error");
+		}
+        
+		return new JedisCluster(configRedisCluster((String) map.get(CLUSTER_NODES)), Integer.valueOf((String) map.get(TIMEOUT)));
 	}
+	
+	private Map<String, Object> loadProperties() {
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+        for (Iterator<PropertySource<?>> it = ((AbstractEnvironment) env).getPropertySources().iterator(); it.hasNext();) {
+            PropertySource<?> propertySource = it.next();
+            getPropertiesFromSource(propertySource, map);
+        }
+        
+        return map;
+	}
+	
+	private void getPropertiesFromSource(PropertySource<?> propertySource, Map<String, Object> map) {
+    	
+        if (propertySource instanceof MapPropertySource) {
+            for (String key : ((MapPropertySource) propertySource).getPropertyNames()) {
+                map.put(key, propertySource.getProperty(key));
+            }
+        }
+        
+        if (propertySource instanceof CompositePropertySource) {
+            for (PropertySource<?> s : ((CompositePropertySource) propertySource).getPropertySources()) {
+                getPropertiesFromSource(s, map);
+            }
+        }
+    }
 	
 	private static Set<HostAndPort> configRedisCluster(String clusterNodes) {
 		
