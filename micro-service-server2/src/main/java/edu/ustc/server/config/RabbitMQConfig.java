e01cbb64e0099450fa636e5aedcc7aa5bc5eab65
colddew <88028842@qq.com>
2015-12-14 14:10:41 +0800

    add rabbitmq publisher confirms mechanism

--- micro-service-server2/src/main/java/edu/ustc/server/config/RabbitMQConfig.java
+++ micro-service-server2/src/main/java/edu/ustc/server/config/RabbitMQConfig.java
@@ -1,12 +1,21 @@
 package edu.ustc.server.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.core.RabbitTemplate.ConfirmCallback;
+import org.springframework.amqp.rabbit.support.CorrelationData;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.backoff.ExponentialBackOffPolicy;
+import org.springframework.retry.support.RetryTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import edu.ustc.server.mq.Receiver;
@@ -16,15 +25,56 @@ import edu.ustc.server.mq.Sender;
 @EnableScheduling
 public class RabbitMQConfig {
 	
+	private static final Logger logger = LoggerFactory.getLogger(RabbitMQConfig.class);
+	
 	@Value("${classes.exchange.name}")
 	private String exchangeName;
 	
 	@Value("${classes.queue.name}")
 	private String queueName;
 	
+	@Bean()
+//	@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+	public RabbitTemplate rabbitTemplate() {
+		
+		ExponentialBackOffPolicy backOffPolicy = new ExponentialBackOffPolicy();
+		backOffPolicy.setInitialInterval(500);
+		backOffPolicy.setMultiplier(10.0);
+		backOffPolicy.setMaxInterval(10000);
+		
+		RetryTemplate retryTemplate = new RetryTemplate();
+		retryTemplate.setBackOffPolicy(backOffPolicy);
+		
+		RabbitTemplate template = new RabbitTemplate(connectionFactory());
+		template.setRetryTemplate(retryTemplate);
+		template.setChannelTransacted(false);
+		
+		template.setConfirmCallback(new ConfirmCallback() {
+			@Override
+			public void confirm(CorrelationData correlationData, boolean ack, String cause) {
+				if(ack) {
+					logger.info("发送消息成功, correlationId={}", correlationData.getId());
+				} else {
+					logger.info("发送消息失败, correlationId={}, cause={}", correlationData.getId(), cause);
+				}
+			}
+		});
+		
+		return template;
+	}
+	
+	@Bean
+	public ConnectionFactory connectionFactory() {
+		
+		CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+		connectionFactory.setPublisherConfirms(true);
+		
+		return connectionFactory;
+	}
+	
 	@Bean
 	public Queue classesQueue() {
-		return new org.springframework.amqp.core.Queue(queueName, true, false, false);
+		return new Queue(queueName, true, false, false);
 	}
 	
 	@Bean
--- micro-service-server2/src/main/java/edu/ustc/server/mq/Sender.java
+++ micro-service-server2/src/main/java/edu/ustc/server/mq/Sender.java
@@ -1,6 +1,11 @@
 package edu.ustc.server.mq;
 
+import java.util.UUID;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.support.CorrelationData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -11,13 +16,16 @@ import edu.ustc.server.pojo.Classes;
 
 public class Sender {
 	
+	private static final Logger logger = LoggerFactory.getLogger(Sender.class);
+	
 	@Value("${classes.exchange.name}")
 	private String exchangeName;
 	
 	@Autowired
 	private RabbitTemplate rabbitTemplate;
 	
-	@Scheduled(cron = "0/2 * *  * * ? ")
+//	@Transactional(propagation = Propagation.NOT_SUPPORTED)
+	@Scheduled(cron = "0/5 * *  * * ? ")
 //	@Scheduled(fixedDelay = 1000 * 60 * 60)
 	public void send() {
 		
@@ -25,6 +33,9 @@ public class Sender {
 		classes.setId(1);
 		classes.setGrade("一年级");
 		
-		rabbitTemplate.convertAndSend(exchangeName, null, JSON.toJSONString(classes));
+		CorrelationData correlationData = new CorrelationData(UUID.randomUUID().toString());
+		logger.info("发送消息, correlationId={}, message={}", correlationData.getId(), JSON.toJSONString(classes));
+		
+		rabbitTemplate.convertAndSend(exchangeName, null, JSON.toJSONString(classes), correlationData);
 	}
 }
