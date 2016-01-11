25be37329e654be3304dc72f93b22d65397734ed
colddew <88028842@qq.com>
2016-01-11 19:47:00 +0800

    test fallback function

--- micro-service-gateway/src/main/java/edu/ustc/gateway/service/MicroService.java
+++ micro-service-gateway/src/main/java/edu/ustc/gateway/service/MicroService.java
@@ -66,7 +66,7 @@ public class MicroService {
 	}
 	
 	public static void main(String[] args) {
-//		MicroService.list();
-		MicroService.update();
+		MicroService.list();
+//		MicroService.update();
 	}
 }
