9e1a69e3360406e08d4bf957ad4fa5c0949c9c8b
colddew <88028842@qq.com>
2017-02-09 14:13:42 +0800

    add Cypher reference

--- micro-service-server/src/main/java/edu/ustc/server/service/ContactService.java
+++ micro-service-server/src/main/java/edu/ustc/server/service/ContactService.java
@@ -66,3 +66,36 @@ public class ContactService {
 		return null;
 	}
 }
+
+
+//@Query("MATCH (providerType:ProviderType {typeCode:{0}, description :{1} }) RETURN providerType")
+//ProviderType findByTypeCodeAndDescription(String typeCode, String description);
+//
+//@Query("MATCH (suburb:Suburb {postcode:{0}, name :{1} }) RETURN suburb")
+//Suburb findSuburbByPostcodeAndSuburb(Long postcode, String name);
+//
+//@Query("MATCH (speciality:Speciality {specialtyCode:{0}, description :{1} }) RETURN speciality")
+//Speciality findBySpecialityCodeAndDescription(String specialityCode, String description);
+//
+//
+//
+//public interface TrackRepository extends GraphRepository<Track>,SpatialRepository<Track> {
+//@Query("match (t:Topic)<--(s:Session)<--(tr:Track) where t.name = {0} and tr.tst = {1} return tr")
+//public Track findByTopicAndTimestamp(String topic, Long timestamp);
+//@Query("match (t:Topic)<--(s:Session)<--(tr:Track) where id(t)={0} return tr")
+//public Result<Track> findAllForTopic(Long topicId);
+//@Query("match (s:Session)<--(tr:Track) where id(s)={0} return tr order by tr.tst")
+//public Result<Track> findAllForSession(Long sessionId);
+//@Query("start n=node:" + Track.wktIndexName + "({0}) match (s:Session)<--(n) where id(s)={1} return s")
+//public Result<Track> findWithinDistanceForSession(String withinDistance, Long sessionId);
+//}
+//
+//
+//public interface SessionRepository extends GraphRepository<Session> {
+//@Query("match (t:Topic)<--(s:Session) where id(t)={0} return s order by s.date desc limit 1")
+//public Session findLatestSession(Long topicId);
+//@Query("match (t:Topic)<--(s:Session) where t.name={0} return s order by s.date desc limit 1")
+//public Session findLatestSession(String topicName);
+//@Query("match (t:Topic)<--(s:Session) where id(t)={0} return s order by s.date desc")
+//public Result<Session> findSessionsByTopicId(Long topicId);
+//}
