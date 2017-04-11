b29d5444f17d3af21a994ee89b7d2c02b2517e46
colddew <88028842@qq.com>
2017-04-11 12:11:37 +0800

    optmize caffeine cache

--- micro-service-server/src/main/java/edu/ustc/server/service/CaffeineCacheService.java
+++ micro-service-server/src/main/java/edu/ustc/server/service/CaffeineCacheService.java
@@ -1,17 +1,15 @@
 package edu.ustc.server.service;
 
-import java.util.concurrent.TimeUnit;
-
-import javax.annotation.PostConstruct;
-
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.LoadingCache;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import com.github.benmanes.caffeine.cache.Caffeine;
-import com.github.benmanes.caffeine.cache.LoadingCache;
+import javax.annotation.PostConstruct;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class CaffeineCacheService {
@@ -19,10 +17,14 @@ public class CaffeineCacheService {
 	private static final Logger logger = LoggerFactory.getLogger(CaffeineCacheService.class);
 	
 	private static LoadingCache<String, String> caffeineCache;
-	
+
 	/**
-	 * refreshAfterWrite will make a key eligible for refresh after the specified duration, 
-	 * but a refresh will only be actually initiated when the entry is queried
+	 * refreshAfterWrite will make a key eligible for refresh after the specified duration,
+	 * but a refresh will only be actually initiated when the entry is queried.
+	 *
+	 * you can specify both refreshAfterWrite and expireAfterWrite on the same cache so that
+	 * the expiration timer on an entry isn't blindly reset whenever an entry becomes eligible for a refresh.
+	 * If an entry isn't queried after it comes eligible for refreshing, it is allowed to expire.
 	 */
 	@PostConstruct
 	public void init() {
