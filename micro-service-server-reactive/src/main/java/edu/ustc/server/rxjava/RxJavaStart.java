cc1c4a42dcdcec4f148c311b664f4f85d81e41a6
colddew <88028842@qq.com>
2019-07-12 01:11:58 +0800

    complete RxJava async logic, solve the problem of no result displayed

--- micro-service-server-reactive/src/main/java/edu/ustc/server/rxjava/RxJavaStart.java
+++ micro-service-server-reactive/src/main/java/edu/ustc/server/rxjava/RxJavaStart.java
@@ -1,10 +1,10 @@
 package edu.ustc.server.rxjava;
 
-import io.reactivex.Observable;
-import io.reactivex.ObservableOnSubscribe;
-import io.reactivex.Observer;
+import io.reactivex.*;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,9 +12,14 @@ public class RxJavaStart {
 
     private static final Logger logger = LoggerFactory.getLogger(RxJavaStart.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
 //        sync();
-        async();
+//        async();
+//        backpressure();
+
+        // 当调用observeOn()或者subscribeOn()后代码运行在子线程，如果子线程还没来得及调用map()和subscribe()主线程就执行完了，有可能是看不到运行结果
+        Thread.sleep(3000);
     }
 
     private static void sync() {
@@ -54,19 +59,24 @@ public class RxJavaStart {
 
     private static void async() {
 
-        Observable.create((ObservableOnSubscribe<String>) emitter -> {
-            logger.info("send thread name: {}", Thread.currentThread().getName());
-            emitter.onNext("async subscribe one ...");
-            emitter.onNext("async subscribe two ...");
-            emitter.onNext("async subscribe three ...");
-            emitter.onComplete();
+        Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(ObservableEmitter<String> emitter) throws Exception {
+                logger.info("send thread name: {}", Thread.currentThread().getName());
+                emitter.onNext("async subscribe one ...");
+                emitter.onNext("async subscribe two ...");
+                emitter.onNext("async subscribe three ...");
+                emitter.onComplete();
+            }
         })
 
         // observable thread
 //        .subscribeOn(Schedulers.io())
+        .subscribeOn(Schedulers.computation())
+//        .subscribeOn(Schedulers.newThread())
 
         // observer thread, allow more than twice
-        .observeOn(Schedulers.trampoline())
+//        .observeOn(Schedulers.trampoline())
         .observeOn(Schedulers.newThread())
 //        .observeOn(AndroidSchedulers.mainThread())
 
@@ -75,6 +85,7 @@ public class RxJavaStart {
 //        })
 
         .subscribe(new Observer<String>() {
+
             @Override
             public void onSubscribe(Disposable d) {
                 logger.info("async subscribe, disposable: {}", d.isDisposed());
@@ -96,5 +107,44 @@ public class RxJavaStart {
                 logger.info("async complete");
             }
         });
+    }
+
+    private static void backpressure() {
+
+        Flowable.create(new FlowableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(FlowableEmitter<Integer> emitter) throws Exception {
+                emitter.onNext(1);
+                emitter.onNext(2);
+                emitter.onNext(3);
+                emitter.onComplete();
+            }
+        }, BackpressureStrategy.ERROR)
+                .subscribeOn(Schedulers.computation())
+                .map(i -> i + 1)
+                .observeOn(Schedulers.newThread())
+                .subscribe(new Subscriber<Integer>() {
+
+                    @Override
+                    public void onSubscribe(Subscription s) {
+                        s.request(Long.MAX_VALUE);
+//                        s.request(3);
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+                        System.out.println(integer);
+                    }
+
+                    @Override
+                    public void onError(Throwable t) {
+                        System.out.println(t.getMessage());
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        System.out.println("complete");
+                    }
+                });
     }
 }
